71e692929a901992cbe81e0f8efaa2dbfd048254
Marc <marcp@prestige-soft.com>
2023-11-16 13:56:46 +0100

    Creacion

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+const name = 'Vue 3';
+const arrayColors = ['blue', 'red'];
+const activo = true;
+</script>
+
+<template>
+  <h1>
+    Hola {{ name.toUpperCase() }}
+  </h1>
+  <br>
+  <h2 :style="`color: ${arrayColors[0]}`">
+    {{ activo ? "activo" : "inactivo" }}
+  </h2>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
