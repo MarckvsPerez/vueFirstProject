a0d999ad2687295311cf0f409dd1c6cef5172b7f
Marc <marcp@prestige-soft.com>
2023-11-20 18:12:50 +0100

    Finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,117 @@
 <script setup>
-import {ref} from 'vue';
+import {ref, computed} from 'vue';
 
 const name = 'Vue 3';
 const cuenta = ref(0);
+const favNumbers = ref([]);
 
-const sumar = () => {
+const sum = () => {
   cuenta.value++;
 };
-const restar = () => {
+const substract = () => {
   cuenta.value--;
 };
-const reiniciar = () => {
+const restart = () => {
   cuenta.value = 0;
 };
+
+const classCounter = computed(() => {
+  if (cuenta.value === 0) return 'zero';
+  if (cuenta.value > 0) return 'positive';
+  else return 'negative';
+});
+
+const enableAddButton = computed(() => {
+  if (favNumbers.value.includes(cuenta.value)) return true;
+  else return false;
+});
+
+const addNumber = () => {
+  favNumbers.value.push(cuenta.value);
+};
+
 </script>
 
 <template>
-  <h1>
-    Hola {{ name.toUpperCase() }}
-  </h1>
-  <h2>{{ cuenta }}</h2>
-  <br>
-  <button @click.right.prevent="sumar">
-    Sumar
-  </button>
-  <button @click.left="restar">
-    Resta
-  </button>
-  <button @click.middle="reiniciar">
-    Reiniciar
-  </button>
+  <div class="title">
+    <h1>
+      Hola {{ name.toUpperCase() }}
+    </h1>
+    <h2 :class="classCounter">
+      Cuenta:
+      {{ cuenta }}
+    </h2>
+  </div>
+  <div class="controller">
+    <button
+      class="btn btn-primary"
+      @click="sum"
+    >
+      Sumar
+    </button>
+    <button
+      class="btn btn-primary"
+      @click="substract"
+    >
+      Resta
+    </button>
+    <button
+      class="btn btn-primary"
+      @click="restart"
+    >
+      Reiniciar
+    </button>
+    <button
+      class="btn btn-success"
+      :disabled="enableAddButton"
+      @click="addNumber"
+    >
+      Añadir
+    </button>
+  </div>
+  <ul class="list-group">
+    <template
+      v-for="number in favNumbers"
+      :key="number"
+    >
+      <li class="list-group-item">
+        {{ number }}
+      </li>
+    </template>
+  </ul>
 </template>
 
 <style>
-h1 {
+div {
+  text-align: center;
+}
+
+li {
+  list-style: none;
+}
+
+.title {
+  text-align: center;
+}
+
+.controller button {
+  margin: 5px;
+}
+
+.positive {
+  color: green;
+}
+
+.negative {
   color: red;
+}
+
+.zero {
+  color: blue;
+}
+
+.list-group {
+  width: 400px;
+  margin: auto;
 }
 </style>
