50ab4d54bd9a700ac919670fb78600ea5bf4f365
Marc Pérez Salat <marcantoniperez@gmail.com>
2023-11-26 21:25:02 +0100

    Seccion4

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <script setup>
-import {ref} from 'vue';
+import {ref, onMounted} from 'vue';
+
 import BlogPost from './components/BlogPost.vue';
+import PaginatedPost from '@/components/PaginatedPost.vue';
+import LoadingSpinner from '@/components/LoadingSpinner.vue';
 
-const posts = ref([
-  {title: 'Titulo 1', body: 'Descripcion 1', colorText: 'primary'},
-  {title: 'Titulo 2', body: 'Descripcion 2', colorText: 'primary'},
-  {title: 'Titulo 3', body: 'Descripcion 3', colorText: 'secondary'},
-]);
+const posts = ref([]);
 
+const postPorPagina = 10;
+const inicio = ref(0);
+const fin = ref(postPorPagina);
 const fav = ref('');
+const loading = ref(true);
+
 const cambiarFavorito = (title) => {
   fav.value = title;
 };
+
+const next = () => {
+  inicio.value += postPorPagina;
+  fin.value += postPorPagina;
+};
+
+const prev = () => {
+  inicio.value += -postPorPagina;
+  fin.value += -postPorPagina;
+};
+
+onMounted(async () => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    posts.value = await res.json();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    setTimeout(() => {
+      loading.value= false;
+    }, 2000);
+  }
+});
+
 </script>
 
 <template>
-  <div class="container">
+  <LoadingSpinner v-if="loading" />
+  <div
+    class="container"
+    v-else
+  >
     <h1>App</h1>
     <h2>Mis post favorito: {{ fav }}</h2>
 
+
+    <PaginatedPost
+      @next-method="next"
+      @prev-method="prev"
+      :inicio="inicio"
+      :fin="fin"
+      :max="posts.length"
+    />
+
     <BlogPost
-      v-for="post in posts"
+      class="mb-2"
+      v-for="post in posts.slice(inicio, fin)"
       :key="post"
       :title="post.title"
       :body="post.body"
@@ -38,4 +80,5 @@ h1 {
   font-weight: 300;
   line-height: 58px;
   margin: 0 0 58px;
-}</style>
+}
+</style>
---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="d-flex justify-content-center mt-5">
+    <div
+      class="spinner-border"
+      role="status"
+    />
+  </div>
+</template>
